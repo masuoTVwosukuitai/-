30c90fe1a10e93b07c774a8ec720893dc40b17ea
masuoTVwosukuitai <148538409+masuoTVwosukuitai@users.noreply.github.com>
2023-10-27 18:57:59 +0900

    Add files via upload

---
+++ kadai3-1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+	float a, b,kansuu;
+	printf("”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
+	scanf_s("%f",&a);
+	printf("”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
+	scanf_s("%f", &b);
+	kansuu= a *b/2;
+	printf("“ñ‚Â‚Ì”š‚ğŠ|‚¯‚Ä‚»‚ê‚ğ‚Q‚ÅŠ„‚Á‚½”‚Í%f", kansuu);
+	return 0;
+}
