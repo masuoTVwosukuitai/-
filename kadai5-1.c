cad15de11b9619734753051ba1bf5a5ae305f4a2
masuoTVwosukuitai <148538409+masuoTVwosukuitai@users.noreply.github.com>
2023-11-10 18:30:20 +0900

    Add files via upload

---
+++ kadai5-1.c
@@ -0,0 +1,6 @@
+﻿ビルドを開始しました...
+1>------ ビルド開始: プロジェクト: sistemsouseikiso5, 構成: Debug x64 ------
+1>galipon5-1.c
+1>sistemsouseikiso5.vcxproj -> C:\Users\t226135\source\repos\sistemsouseikiso5\x64\Debug\sistemsouseikiso5.exe
+========== ビルド: 成功 1、失敗 0、最新の状態 0、スキップ 0 ==========
+========== ビルド は 18:24 に開始され、00.551 秒 かかりました ==========
