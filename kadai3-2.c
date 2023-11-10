cff9d92d091acf56b789fa0f9e767cefb3bf7d6b
masuoTVwosukuitai <148538409+masuoTVwosukuitai@users.noreply.github.com>
2023-11-10 18:35:41 +0900

    Add files via upload

--- kadai3-2.c
+++ kadai3-2.c
@@ -1,1 +1,19 @@
-ï»¿
+#include<stdio.h>
+int main(void)
+{
+	int a;
+	printf("”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
+	scanf_s("%d", &a);
+	printf("‚ ‚È‚½‚Ì“ü—Í‚µ‚½”š‚Í");
+	if (a % 2 == 0)
+	{
+		printf("‹ô”");
+	}
+	else
+	{
+		printf("Šï”");
+	}
+	printf("‚Å‚·B");
+	return 0;
+	
+}
