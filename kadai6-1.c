2b7c0264d4e27390ca5205dd1b75f78486426c84
masuoTVwosukuitai <148538409+masuoTVwosukuitai@users.noreply.github.com>
2023-11-17 18:24:16 +0900

    Add files via upload

--- kadai6-1.c
+++ kadai6-1.c
@@ -1,11 +1,14 @@
-﻿ビルドを開始しました...
-1>------ ビルド開始: プロジェクト: kadai6-1, 構成: Debug x64 ------
-1>kadai6-1.c
-1>C:\Users\t226135\source\repos\kadai6-1\kadai6-1\kadai6-1.c(6,10): warning C4473: 'scanf_s': 書式文字列として渡された引数が不足しています
-1>C:\Users\t226135\source\repos\kadai6-1\kadai6-1\kadai6-1.c(6,10): message : プレースホルダーとそのパラメーターには 2 の可変個引数が必要ですが、1 が指定されています。
-1>C:\Users\t226135\source\repos\kadai6-1\kadai6-1\kadai6-1.c(6,10): message : 不足している可変個引数 2 が書式文字列 '%c' に必要です
-1>C:\Users\t226135\source\repos\kadai6-1\kadai6-1\kadai6-1.c(6,10): message : この引数はバッファー サイズとして使用されます
-1>kadai6-1.vcxproj -> C:\Users\t226135\source\repos\kadai6-1\x64\Debug\kadai6-1.exe
-1>プロジェクト "kadai6-1.vcxproj" のビルドが終了しました。
-========== ビルド: 成功 1、失敗 0、最新の状態 0、スキップ 0 ==========
-========== ビルド は 18:20 に開始され、00.674 秒 かかりました ==========
+#include<stdio.h>
+int main(void)
+{
+	char a;
+	printf("��������͂��Ă��������F");
+	scanf_s("%c", &a);
+	if ((a >= 'A') && (a <= 'Z')) {
+		printf("�啶���͊܂܂�Ă��܂�\n");
+	}
+	else {
+		printf("�啶���͊܂܂�Ă��܂���");
+	}
+	return 0;
+}
